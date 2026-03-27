60b2bf8eaac625b7efbe66a623fdb0c3240cea79
Jacob Thomas <jdthomas2000@gmail.com>
2026-03-27 12:00:27 -0700

    got basic graph with gdp to show up

--- ui/frontend/src/GDP.jsx
+++ ui/frontend/src/GDP.jsx
@@ -10,6 +10,18 @@ import {
   useParams,
 } from "react-router-dom";
 
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { RechartsDevtools } from "@recharts/devtools";
+
 export default function Graph({ countryData }) {
   const [gdpData, setGdpData] = useState(null);
 
@@ -23,11 +35,67 @@ export default function Graph({ countryData }) {
 
   if (!gdpData) return <h1>data loading... or missing maybe</h1>;
 
+  const compactFormatter = new Intl.NumberFormat("en-US", {
+    notation: "compact",
+    compactDisplay: "short",
+  });
+
+  const data = [];
+
+  for (let i = 2000; i < 2025; i++) {
+    const dataObj = { year: null, gdp: null };
+
+    dataObj.year = i;
+    dataObj.gdp = Number(gdpData[`gdp_${i}`]) || 0;
+
+    data.push(dataObj);
+  }
+
+  // console.log(data);
+
   return (
-    <>
-      <h1>
-        {gdpData.name} {gdpData.country_code} {gdpData.gdp_2000}
-      </h1>
-    </>
+    <div className="graph">
+      <h3 className="graph-title">
+        GDP Growth: {gdpData.name} ({gdpData.country_code})
+      </h3>
+      <div className="chart-container">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={data}
+            margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid
+              strokeDasharray="3 3"
+              stroke="var(--color-border-3)"
+            />
+
+            <XAxis dataKey="year" stroke="var(--color-text-3)" />
+
+            <YAxis
+              dataKey="gdp"
+              stroke="var(--color-text-3)"
+              tickFormatter={(val) => compactFormatter.format(val)}
+            />
+
+            <Tooltip
+              formatter={(val) => compactFormatter.format(val)}
+              cursor={{ stroke: "var(--color-border-2)" }}
+              contentStyle={{
+                backgroundColor: "var(--color-surface-raised)",
+                borderColor: "var(--color-border-2)",
+              }}
+            />
+
+            <Line
+              type="monotone"
+              dataKey="gdp" // Changed to lowercase "gdp"
+              stroke="var(--color-chart-1)"
+              dot={{ fill: "var(--color-surface-base)" }}
+              activeDot={{ r: 8, stroke: "var(--color-surface-base)" }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
   );
 }
